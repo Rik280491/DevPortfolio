ab2f027fc32ff778f7b269f017427a283acfba68
RikBala <rik280491@googlemail.com>
2020-06-09 15:38:43 -0700

    background image

---
+++ src/components/landing-page/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function LandingPage(props) {
+
+return (
+    <div className="backgroundImage">
+        <h1>TEST</h1>
+    </div>
+
+
+)
+}
+
+export default LandingPage
