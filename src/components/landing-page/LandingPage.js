9622fc538e4ca0d26c65cbd2caaa7de7706d27cc
RikBala <rik280491@googlemail.com>
2020-06-09 15:58:02 -0700

    much cooler picture and header

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -1,16 +1,13 @@
-import React from 'react'
-
-
+import React from 'react';
+import Header from '../header/Header'
 
 function LandingPage(props) {
-
-return (
-    <div className="backgroundImage">
-        <h1>TEST</h1>
-    </div>
-
-
-)
+	return (
+		<div className="backgroundImage">
+            <Header/>
+			
+		</div>
+	);
 }
 
-export default LandingPage
+export default LandingPage;
