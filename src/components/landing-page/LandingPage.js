c540c3827604bdacc9ffced21bd0eaa178320c4b
RikBala <rik280491@googlemail.com>
2020-06-14 12:08:12 -0700

    merge

--- src/components/landing-page/LandingPage.js
+++ src/components/landing-page/LandingPage.js
@@ -4,7 +4,7 @@ import Header from '../header/Header';
 function LandingPage(props) {
 	return (
 		<div className="backgroundImage">
-            {/* slightly hacky but ensures a11y to the div image */}
+            {/* slightly hacky but ensures div image a11y */}
             <span role="img" aria-label="A photograph of a Typewriter. Decorative Image"> </span>
             <Header />
 		</div>
