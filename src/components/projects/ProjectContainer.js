1efe9de608ff2e3b7629070a5ffa7bb91bd28e2b
RikBala <rik280491@googlemail.com>
2020-06-26 23:49:58 +0100

    added logos

--- src/components/projects/ProjectContainer.js
+++ src/components/projects/ProjectContainer.js
@@ -2,32 +2,51 @@ import React from "react";
 import HearTogether from "./hearTogether/HearTogether";
 import MusicPlayer from "./musicPlayer/MusicPlayer";
 import WhoSaidIt from "./whoSaidIt/WhoSaidIt";
-import { Grid } from "semantic-ui-react";
-
-
-const styles = {
-	display: 'flex', 
-	justifyContent: 'center'
-	
-};
+import { Grid, Rail } from "semantic-ui-react";
 
 function ProjectContainer() {
 	return (
-		<div id="projects" >
-            <h1>Projects</h1>
+		<div id="projects">
+			<h1>Projects</h1>
 			<br></br>
-			<Grid columns={3} style={styles} >
+			<Grid centered columns={3}>
 				<Grid.Row>
 					<Grid.Column>
 						<HearTogether />
-						<br></br>
-						<MusicPlayer />
-						<br></br>
-						<WhoSaidIt />
+						<Rail dividing position="left">
+							<i class="devicon-rails-plain-wordmark colored"></i>
+						</Rail>
+
+						<Rail dividing position="right">
+							<i class="devicon-react-original-wordmark colored"></i>
+						</Rail>
 					</Grid.Column>
 				</Grid.Row>
-            </Grid>
-           
+				<Grid.Row>
+					<Grid.Column>
+						<MusicPlayer />
+						<Rail dividing position="left">
+							<i class="devicon-rails-plain-wordmark colored"></i>{" "}
+						</Rail>
+
+						<Rail dividing position="right">
+							<i class="devicon-react-original-wordmark colored"></i>
+						</Rail>
+					</Grid.Column>
+				</Grid.Row>
+				<Grid.Row>
+					<Grid.Column>
+						<WhoSaidIt />
+						<Rail dividing position="left">
+							<i class="devicon-rails-plain-wordmark colored"></i>{" "}
+						</Rail>
+
+						<Rail dividing position="right">
+							<i class="devicon-javascript-plain colored"></i>
+						</Rail>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
 		</div>
 	);
 }
